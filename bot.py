40a0315b0fd82755894d9b6ffe0ffaa4263d6a05
BEN ABDALLAH Khaled <khaled.ben-abdallah@etu.univ-amu.fr>
2023-11-01 20:40:05 +0100

    Ajout météo

--- bot.py
+++ bot.py
@@ -7,18 +7,37 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+METEO_TOKEN = os.getenv('METEO_TOKEN')
 
 client = discord.Client(intents=discord.Intents.default())
 
-
-def get_quote():
-  response = requests.get("https://zenquotes.io/api/random")
+def pluie():
+  response = requests.get(f"https://api.weatherapi.com/v1/forecast.json?key={METEO_TOKEN}&q=Paris&days=1&aqi=no&alerts=no")
   json_data = json.loads(response.text)
-  quote = json_data[0]['q'] + " -" + json_data[0]['a']
-  return(quote)
+  #print(json_data)
+  pluie = json_data["forecast"]["forecastday"][0]["day"]["daily_will_it_rain"]
+  chance = json_data["forecast"]["forecastday"][0]["day"]["daily_chance_of_rain"]
+  return (int(pluie), int(chance))
 
 @client.event
 async def on_ready():
     print(f"{client.user} s'est connecté, écartez vous s'il vous plaît")
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    #message = message.lower();
+    #if message.content.startswith('$meteo'):
+    if "météo" or "meteo" in message.content.lower():
+        if pluie()[0]:
+            meteo = f"Attention les enfants il risque de pleuvoir avec {pluie()[1]}% de chance demain, n'oubliez pas votre parapluie."
+
+        else:
+            meteo = "Il ne devrait pas pleuvoir demain."
+
+        await message.channel.send(meteo)
+
+pluie()
 client.run(TOKEN)
